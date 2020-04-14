bfe19061e9984a51e22215013c3e35f0e623fdd1
Patrick Skelley <songkick.skelley@gmail.com>
2020-04-13 20:42:01 -0400

    switches from java to xml configuration, check diff of previous commit to switch from one to the other

--- src/main/java/patty/springframework/joke/jokeapp/JokeappApplication.java
+++ src/main/java/patty/springframework/joke/jokeapp/JokeappApplication.java
@@ -2,8 +2,11 @@ package patty.springframework.joke.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")  // needed for xml config of spring beans.
+// comment this line and uncomment annotations within ChuckConfig to switch back to java configuration 
 public class JokeappApplication {
 
     public static void main(String[] args) {
